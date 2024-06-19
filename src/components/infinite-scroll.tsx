154794c92ca87b8b105a24695f97f80578ef9b74
jeongho <hyeon9782@gmail.com>
2024-06-19 23:28:24 +0900

    feat: infinite scroll 기능 구현

--- src/components/infinite-scroll.tsx
+++ src/components/infinite-scroll.tsx
@@ -1,12 +1,44 @@
+import { useEffect, useRef, useState } from "react";
 import "./infinite-scroll.css";
 import ListItem from "./list-item";
 
 const InfiniteScroll = () => {
+  const ref = useRef(null);
+  const [data, setData] = useState(Array.from({ length: 10 }));
+  const [page, setPage] = useState(1);
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    setData((prevData) => [...prevData, ...Array.from({ length: 10 })]);
+  }, [page]);
+
+  const handleIntersect = (entries: IntersectionObserverEntry[]) => {
+    const [entry] = entries;
+    if (entry.isIntersecting && !isLoading) {
+      setPage((prevPage) => prevPage + 1);
+    }
+  };
+
+  const observer = new IntersectionObserver(handleIntersect, {
+    root: null,
+    rootMargin: "200px",
+    threshold: 0.5,
+  });
+
+  useEffect(() => {
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => observer.unobserve(ref.current);
+  }, [ref]);
+
   return (
     <div className="container">
-      {Array.from({ length: 20 }).map((_, index) => (
+      {data.map((_, index) => (
         <ListItem key={index} index={index} />
       ))}
+      <div ref={ref}></div>
     </div>
   );
 };
